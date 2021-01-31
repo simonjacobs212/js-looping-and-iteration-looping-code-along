2687c832bb38193050fba3a5ab2aa5b01dc273cd
Simon Jacobs <simonadlerjacobs@gmail.com>
2021-01-30 19:06:12 -0500

    commit tree 7ad12490a850035e76d49ed37043895deaba2cdd

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const people = ["Ada", "Brendan", "Ali"]
+
+function writeCards(people, event){
+    let new_arr = []
+    for (let i = 0; i < people.length; i++){
+        new_arr.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`); 
+    }
+    return new_arr
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num--)
+    }
+}
